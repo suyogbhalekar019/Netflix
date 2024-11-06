9335ee43df93b2e53417310cab3a6cc88511fcab
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-11-06 16:53:25 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,6 +1,6 @@
 locals {
   region = "ap-south-1"
-  name   = "amonkincloud-cluster"
+  name   = "eks-cloud-cluster"
   vpc_cidr = "10.123.0.0/16"
   azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
