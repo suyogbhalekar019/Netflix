6f4de4e5e8de61df4256256afb56391dee36a7df
Suyog_Bhalekar <152723284+suyogbhalekar019@users.noreply.github.com>
2024-11-06 16:53:53 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "ap-south-1"
   name   = "eks-cloud-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
+  azs      = ["api-south-1a", "api-south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
